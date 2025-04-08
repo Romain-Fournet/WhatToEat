831b4e201df39abfff73f1e1942623262ec857d0
Romain Fournet <fournet.romain79@gmail.com>
2025-04-08 17:47:21 +0200

    fix : add cookie and change session getter

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -9,51 +9,22 @@ interface User {
   emailVerified: boolean;
   createdAt: Date;
   updatedAt: Date;
-  image?: string | null | undefined | undefined;
+  image?: string | null;
 }
 
 export default function UserCard() {
   const [user, setUser] = useState<User | null>(null);
 
-  const {
-    data: session,
-    isPending, // loading state
-    error, // error object
-  } = authClient.useSession();
-
-  // Mettre à jour l'état de l'utilisateur une fois la session chargée
   useEffect(() => {
-    if (!isPending && session) {
-      setUser(session.user);
-    }
-  }, [session, isPending]); // Ce useEffect sera déclenché à chaque fois que `session` ou `isPending` change.
+    authClient.getSession().then((session) => {
+      if (session.data?.user) {
+        setUser(session.data.user as User);
+        console.log("Session fetched");
+      }
+    });
+  }, []);
 
-  // Afficher un message de chargement si la session est en cours de récupération
-  if (isPending) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-lg font-semibold">Chargement...</p>
-      </div>
-    );
-  }
-
-  // Afficher un message d'erreur si une erreur se produit
-  if (error) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-lg font-semibold">Erreur : {error.message}</p>
-      </div>
-    );
-  }
-
-  // Afficher un message si l'utilisateur n'est pas trouvé
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-lg font-semibold">Utilisateur non trouvé</p>
-      </div>
-    );
-  }
+  if (!user) return <div>Chargement...</div>;
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">

cd6483cbdc448a41411c17aedbba6473323407dc
Romain Fournet <fournet.romain79@gmail.com>
2025-04-09 10:26:28 +0200

    refactor : express for backend

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,10 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
-import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
-
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -13,5 +11,4 @@ export const auth = betterAuth({
     enabled: true,
   },
   trustedOrigins: ["http://localhost:5173"],
-  plugins: [nextCookies()], // doit être le dernier plugin
 });
---
+++ backend/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors"; // Import the CORS middleware
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth";
+
+const app = express();
+const port = 8000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
+app.all("/api/auth/*path", toNodeHandler(auth));
+
+// Mount express json middleware after Better Auth handler
+// or only apply it to routes that don't interact with Better Auth
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
